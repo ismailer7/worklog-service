0a0678cfe776b15a2b0cf4b91486da01c456b98d
Ismail <ismailrzouki7@gmail.com>
2020-06-27 23:49:59 +0200

    setup React project

---
+++ worklog-client/src/component/Login.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+
+
+    render() {
+        return (
+            <h1>
+                Hello Login Component!
+            </h1>
+        )
+    }
+
+}
+
+export default Login;
